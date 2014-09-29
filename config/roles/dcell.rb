88c11232ac47421cb6b124ff33cd41408fe44e68
Yegorka <yegor.tsirkun@gmail.com>
2014-09-29 23:50:34 +0400

    ya v dotku, po rilu, ya zaebalsya

--- config/roles/dcell.rb
+++ config/roles/dcell.rb
@@ -2,7 +2,6 @@ application.on(:start) do
   application.server do 
     Phoenix::Servers::DCell::Server.new.tap do |server|
       server.start(lambda do |call|
-        p call
         ::Phoenix.application.dispatcher.dispatch(call)
       end)
     end
