1ffac5b374a602f84a4dfb8249afe75830cfae0b
Anton Zhuravsky <mail2lf@gmail.com>
2014-10-21 00:30:25 +0300

    Ahahha

--- config/roles/dcell.rb
+++ config/roles/dcell.rb
@@ -1,7 +1,7 @@
 application.on(:start) do
   application.server do 
     Phoenix::Servers::DCell::Server.new.tap do |server|
-      server.start Phoenix::Servers::DCell::Handler.new(::Phoenix.application.dispatcher).method(:handle)
+      server.start Phoenix::Servers::DCell::Handler.new.method(:handle)
     end
   end
 end
--- config/roles/tcp.rb
+++ config/roles/tcp.rb
@@ -1,7 +1,7 @@
 application.on(:start) do
   application.server do 
     Phoenix::Servers::TCP::Server.new.tap do |server|
-      server.start Phoenix::Servers::TCP::Handler.new(::Phoenix.application.dispatcher).method(:handle)
+      server.start Phoenix::Servers::TCP::Handler.new.method(:handle)
     end
   end
 
