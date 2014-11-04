e5206d8602d822ce85ed5526f28d1b6e770729f1
stopiccot <alexey.petruchik@gmail.com>
2014-11-04 14:14:08 +0300

    test random field

--- app/entities/game/base/match.rb
+++ app/entities/game/base/match.rb
@@ -17,23 +17,8 @@ module Entities
         def initialize
           @state = Messages::Game::Base::MatchState.new(
             field: Messages::Game::Base::Field.new(
-              cells: [
-                Messages::Game::Base::Cell.new(id: 0,  playerId: 1, armySize: 1),
-                Messages::Game::Base::Cell.new(id: 1,  playerId: 1, armySize: 2),
-                Messages::Game::Base::Cell.new(id: 2,  playerId: 1, armySize: 3),
-                Messages::Game::Base::Cell.new(id: 3,  playerId: 1, armySize: 4),
-                Messages::Game::Base::Cell.new(id: 4,  playerId: 1, armySize: 5),
-                Messages::Game::Base::Cell.new(id: 5,  playerId: 1, armySize: 6),
-                Messages::Game::Base::Cell.new(id: 6,  playerId: 2, armySize: 1),
-                Messages::Game::Base::Cell.new(id: 7,  playerId: 2, armySize: 2),
-                Messages::Game::Base::Cell.new(id: 8,  playerId: 2, armySize: 3),
-                Messages::Game::Base::Cell.new(id: 9,  playerId: 2, armySize: 4),
-                Messages::Game::Base::Cell.new(id: 10, playerId: 2, armySize: 5),
-                Messages::Game::Base::Cell.new(id: 11, playerId: 2, armySize: 6),
-              ],
-              nodes: [
-
-              ]
+              cells: (0..43).map { |x| Messages::Game::Base::Cell.new( id: x,  playerId: x < 22 ? 1 : 2, armySize: rand(8) + 1 ) },
+              nodes: []
             ),
             players: Messages::Game::Base::MatchPlayers.new(),
             turnNumber: 100,
@@ -42,7 +27,11 @@ module Entities
         end
 
         #  Result is:
-        #    Messages::Game::Base::MatchState( Messages::Game::Base::Field( Messages::Game::Base::Cell( int32 id, int32 playerId, int32 armySize )[] cells, Messages::Game::Base::Node( int32 from, int32 to )[] nodes ) field, Messages::Game::Base::MatchPlayers( Entities::Game::Base::Player[] players ) players, int32 turnNumber, int32 turnPlayerId )
+        #    Messages::Game::Base::MatchState(
+        #      Messages::Game::Base::Field(
+        #         Messages::Game::Base::Cell( int32 id, int32 playerId, int32 armySize )[] cells,
+        #         Messages::Game::Base::Node( int32 from, int32 to )[] nodes
+        #      ) field, Messages::Game::Base::MatchPlayers( Entities::Game::Base::Player[] players ) players, int32 turnNumber, int32 turnPlayerId )
         #
         def get_state
           return @state
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,2 +1,2 @@
 Phoenix.application.logger.debug "Development environment (config = #{ config }) at your service!"
-# Phoenix.reload_code_upon_change!
+Phoenix.reload_code_upon_change!
