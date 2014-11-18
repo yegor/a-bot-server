c14b779db52deedad37189a43d61b46dde0ada2b
stopiccot <alexey.petruchik@gmail.com>
2014-11-18 17:29:31 +0300

    monkey patch celluloid-io

--- config/roles/tcp.rb
+++ config/roles/tcp.rb
@@ -1,3 +1,42 @@
+require 'celluloid/io'
+
+Celluloid::IO::Stream.class_eval do
+  # System write via the nonblocking subsystem
+  def syswrite(string)
+    length = string.length
+    total_written = 0
+
+    remaining = string
+
+    @write_latch.synchronize do
+      while total_written < length
+        begin
+          p "syswriting #{remaining.size} bytes to #{@socket}"
+          written = write_nonblock(remaining)
+          p "#{written} bytes actually written"
+        rescue ::IO::WaitWritable
+          wait_writable
+          retry
+        rescue EOFError
+          return total_written
+        rescue Errno::EAGAIN
+          wait_writable
+          retry
+        end
+
+        total_written += written
+        p "total_written is now #{total_written}"
+
+        # FIXME: mutating the original buffer here. Seems bad.
+        remaining.slice!(0, written) if written < remaining.length
+      end
+    end
+
+    total_written
+  end
+
+end
+
 application.on(:start) do
   application.server do 
     Phoenix::Servers::TCP::Server.new.tap do |server|
