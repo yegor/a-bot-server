f934c54d02bd6cb04604295840eae57b98374018
Anton Zhuravsky <mail2lf@gmail.com>
2014-12-06 21:05:10 +0300

    Adding CONT signal handler

--- config/application.rb
+++ config/application.rb
@@ -4,5 +4,10 @@ ENV["PHOENIX_ROOT"] ||= File.expand_path( File.join(__FILE__, "..", "..") )
 require "phoenix/boot.rb"
 
 stop = false
-Signal.trap('INT') { stop = true }
-sleep(1) until stop
+
+Signal.trap("CONT") do 
+  Phoenix.application.stop
+  stop = true
+end
+
+sleep(1.0) until stop
