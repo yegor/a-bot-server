a6be07af299ac601cb84772ba390e1a6e7bbaf24
Anton Zhuravsky <mail2lf@gmail.com>
2014-12-06 23:32:07 +0300

    Fixing staging recipe

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ servers_ips("app", "staging").each do |ip|
   server(ip, roles: %w(app db), user: fetch(:user), phoenix: {
     instances: [ 
       {id: "db",  count: 1, roles: %w(dcell db),  ports: {dcell: 10000}}, 
-      {id: "manager", count: 1, roles: %w(dcell tcp login match_maker), ports: {dcell: 11000, tcp: 20000}}
+      {id: "manager", count: 1, roles: %w(dcell tcp login match_maker), ports: {dcell: 11000, tcp: 20000}},
       {id: "app", count: 3, roles: %w(dcell tcp), ports: {dcell: 11000, tcp: 30000}}
     ]
   })
